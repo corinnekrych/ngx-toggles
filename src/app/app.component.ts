45d19bcb94bd9bbfa95c34adabd6cdece86eeb8e
Corinne Krych <corinnekrych@gmail.com>
2018-02-27 09:32:58 +0100

    toggles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,39 @@ import {Component, ViewContainerRef} from '@angular/core';
 import {LoaderService} from './service/loader.service';
 import {FeatureAComponent} from './featureA/featureA.component';
 import {Type} from '@angular/compiler/src/core';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
+export class ToggleModel {
+  mode: boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'ngx-toggles';
+  toggle = {mode: false} as ToggleModel;
+  toggleSubject: BehaviorSubject<ToggleModel>;
+
   constructor(private service: LoaderService, private viewContainerRef: ViewContainerRef) {
+    // where to create the component
     service.setRootViewContainerRef(viewContainerRef);
+
+    this.toggle.mode = false;
+    this.toggleSubject = new BehaviorSubject<ToggleModel>(this.toggle);
+    this.toggleSubject.subscribe(value => {
+      console.log(`Toggle changed to ${value.mode}`);
+      if (this.toggle.mode) {
+        this.service.addComponentDynamically(FeatureAComponent as Type);
+      } else {
+        this.service.removeComponent(FeatureAComponent as Type);
+      }
+    });
   }
-  loadFeatureA() {
-    this.service.addComponentDynamically(FeatureAComponent as Type);
+  toggleChanged() {
+    console.log(`Toggle is ${this.toggle.mode}`);
+    this.toggle.mode = !this.toggle.mode;
+    this.toggleSubject.next(this.toggle);
   }
 }
--- src/app/service/loader.service.ts
+++ src/app/service/loader.service.ts
@@ -3,6 +3,7 @@ import {Injectable, Type, ComponentFactoryResolver, ViewContainerRef, Component}
 @Injectable()
 export class LoaderService {
   private rootViewContainer: ViewContainerRef;
+  private addedComponents = [];
 
   constructor(private factoryResolver: ComponentFactoryResolver) {}
 
@@ -13,6 +14,19 @@ export class LoaderService {
   addComponentDynamically(type: Type<Component>) {
     const factory = this.factoryResolver.resolveComponentFactory(type);
     const component = factory.create(this.rootViewContainer.parentInjector);
+    this.addedComponents.push(component);
     this.rootViewContainer.insert(component.hostView);
   }
+  removeComponent(type: Type<Component>) {
+    const index = this.addedComponents.findIndex((c) => c ? c.instance instanceof type : false);
+    if (this.addedComponents[index]) { // if this is an added component
+      const componentIndex = this.rootViewContainer.indexOf(this.addedComponents[index]);
+      console.log(`index: ${index}`);
+      if (componentIndex >= 0) { // delete form the view
+        this.rootViewContainer.remove(this.rootViewContainer.indexOf(this.addedComponents[index]));
+      }
+      delete this.addedComponents[index]; // delete in the list of added components
+    }
+  }
+
 }
